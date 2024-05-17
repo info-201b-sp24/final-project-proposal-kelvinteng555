de031db9a0ee8e2b64fb6d54d853ed7b557f9cb9
October Allen <97641424+lucya4@users.noreply.github.com>
2024-05-17 14:09:04 -0700

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,50 @@
 
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## loading data
+```{r}
+library(tidyverse)
+library(dplyr)
+
+## url to raw data page of gayborhood data on github
+data_url <- "https://raw.githubusercontent.com/the-pudding/data/master/gayborhoods/data.csv"
+
+## reading the data into a dataset for analysis using the raw data url
+dataset <- read.csv(data_url)
+```
+
+## Value 1: Ratio of married FF couples to unmarried FF couples per zip code
+
+```{r}
+```
+## Value 2: Ratio of married MM couples to unmarried MM couples per zip code
+
+```{r}
+```
+## Value 3: % of total FF couples that are married
+
+```{r}
+```
+## Value 4: % of total MM couples that are married
+
+```{r}
+```
+## Value 5: ratio of % FF married couples vs % of MM married couples
+
+```{r}
+```
+## Value 6: number of zipcodes over 60% FF
+
+```{r}
+```
+## Value 7: number of zipcodes over 60% MM
+
+```{r}
+```
+## Value 8: ratio of zipcodes over 60% FF to zipcodes over 60% MM
+
+```{r}
+```
