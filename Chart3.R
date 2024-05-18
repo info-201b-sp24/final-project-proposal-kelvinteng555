7fd753fb7639ec05def051e357771d515af1a6f0
joshlee0 <166346380+joshlee0@users.noreply.github.com>
2024-05-18 06:57:23 -0700

    Update Chart3.R

--- Chart3.R
+++ Chart3.R
@@ -1,1 +1,17 @@
-
+library(ggplot2)
+library(dplyr)
+library(readr)
+dataset <- read_csv("Downloads/data.csv")
+top5 <- dataset %>%
+    top_n(5, TaxRate_SS) %>%
+    arrange(TaxRate_SS)
+zipCode <- top5 %>%
+    pull(GEOID10)
+ggplot(top5)+
+  ggplot(top5)+
+    geom_col(mapping = aes(x = zipCode, y = TaxRate_SS))+
+    labs(
+         title = "Zipcodes versus Highest Rates of SS Couples",
+         x = "Zipcodes",
+         y = "Rate of SS Married Couples"
+         )
