e0f2d1610aca74cef596e1e7519c4d964a58a5f1
joshlee0 <166346380+joshlee0@users.noreply.github.com>
2024-05-18 06:55:52 -0700

    Update Chart2.R

--- Chart2.R
+++ Chart2.R
@@ -1,1 +1,16 @@
-
+library(ggplot2)
+library(dplyr)
+library(readr)
+dataset <- read_csv("Downloads/data.csv")
+top5 <- dataset %>%
+    top_n(5, Cns_TotHH) %>%
+    arrange(TaxRate_SS)
+zipCode <- top5 %>%
+    pull(GEOID10)
+ggplot(dataset)+
+    geom_smooth(mapping = aes(x = CountBars, y = Cns_UPSS))+
+    labs(
+         title = "Number of Gay Bars vs. Unmarried Same-sex Couples",
+         x = "Number of Gay Bars",
+         y = "Unmarried SS Couples"
+         )
