19b68195db979ce5d3a84c212adc45918edf6a86
joshlee0 <166346380+joshlee0@users.noreply.github.com>
2024-05-18 05:44:18 -0700

    Update Chart1.R

--- Chart1.R
+++ Chart1.R
@@ -1,1 +1,17 @@
+library(ggplot2)
+library(dplyr)
+library(readr)
+dataset <- read_csv("Downloads/data.csv")
+top5 <- dataset %>%
+    top_n(5, Cns_TotHH) %>%
+    arrange(TaxRate_SS)
+zipCode <- top5 %>%
+    pull(GEOID10)
+chart1 <- ggplot(top5)+
+  geom_col(mapping = aes(x = zipCode, y = TaxRate_SS))+
+  labs(
+    title = "Most Populated Zipcodes versus Rate of Same-Sex Married Couples",
+    x = "Most Populated Zipcodes",
+    y = "Rate of Same-Sex Married Couples"
+    )
 
